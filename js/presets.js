f529f37ae58c5fe702661bfcc02d4888cb216032
Sadjad Tavakoli <sadjadtavakoli@gmail.com>
2026-04-07 09:06:31 -0700

    Add dimensional lumber and sheet goods presets

---
+++ js/presets.js
@@ -0,0 +1,21 @@
+export const DIMENSIONAL_PRESETS = [
+  { name: '1x2 8ft', type: 'dimensional', length: 96, width: 1.5, thickness: 0.75, price: 3.00, quantity: null },
+  { name: '1x3 8ft', type: 'dimensional', length: 96, width: 2.5, thickness: 0.75, price: 4.50, quantity: null },
+  { name: '1x4 8ft', type: 'dimensional', length: 96, width: 3.5, thickness: 0.75, price: 5.50, quantity: null },
+  { name: '1x6 8ft', type: 'dimensional', length: 96, width: 5.5, thickness: 0.75, price: 8.00, quantity: null },
+  { name: '1x8 8ft', type: 'dimensional', length: 96, width: 7.25, thickness: 0.75, price: 11.00, quantity: null },
+  { name: '1x10 8ft', type: 'dimensional', length: 96, width: 9.25, thickness: 0.75, price: 14.00, quantity: null },
+  { name: '1x12 8ft', type: 'dimensional', length: 96, width: 11.25, thickness: 0.75, price: 18.00, quantity: null },
+  { name: '2x4 8ft', type: 'dimensional', length: 96, width: 3.5, thickness: 1.5, price: 4.00, quantity: null },
+  { name: '2x6 8ft', type: 'dimensional', length: 96, width: 5.5, thickness: 1.5, price: 6.50, quantity: null },
+  { name: '2x8 8ft', type: 'dimensional', length: 96, width: 7.25, thickness: 1.5, price: 9.00, quantity: null },
+  { name: '2x10 8ft', type: 'dimensional', length: 96, width: 9.25, thickness: 1.5, price: 12.00, quantity: null },
+  { name: '2x12 8ft', type: 'dimensional', length: 96, width: 11.25, thickness: 1.5, price: 16.00, quantity: null },
+];
+
+export const SHEET_PRESETS = [
+  { name: '1/4" Plywood 4x8', type: 'sheet', length: 96, width: 48, thickness: 0.25, price: 25.00, quantity: null },
+  { name: '1/2" Plywood 4x8', type: 'sheet', length: 96, width: 48, thickness: 0.5, price: 35.00, quantity: null },
+  { name: '3/4" Plywood 4x8', type: 'sheet', length: 96, width: 48, thickness: 0.75, price: 45.00, quantity: null },
+  { name: '3/4" MDF 4x8', type: 'sheet', length: 96, width: 48, thickness: 0.75, price: 38.00, quantity: null },
+];
